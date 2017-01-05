ad4b4a6647e0fb0ac56df23ea47575d4755f8478
unknown <www3@inbox.com>
2017-01-05 20:28:35 +0200

    Add test files

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.use(express.static('public'));
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
